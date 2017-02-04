1936c939a2d692f1895eef44a10a661637145bcb
Meng Chen <cm369575409@qq.com>
2017-02-04 15:14:17 +0800

    bean config file

---
+++ config/CDPlayerConfig.java
@@ -0,0 +1,9 @@
+@Configuration
+@ComponentScan("package")
+@ComponentScan(basePackages = {"configcenterweb_v6", "else"})
+public class CDPlayerConfig {
+     /*
+        JavaConfig配置文件
+        配置文件自动装配、遍历所有的Component，并自动创建对应的Bean
+     */
+}
